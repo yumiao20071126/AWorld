7199a6f50d8822f39a6206366155f7df510cafe1
ganrunsheng <ganrunsheng@antgroup.com>
2025-04-02 15:51:02 +0800

    browser conf

--- aworld/apps/browsers/run.py
+++ aworld/apps/browsers/run.py
@@ -15,12 +15,16 @@ from aworld.config.conf import ModelConfig
 
 if __name__ == '__main__':
     client = Client()
-    inner_llm_model_config=ModelConfig(
+    inner_llm_model_config = ModelConfig(
         llm_provider="openai",
         llm_model_name="gpt-4o",
         llm_temperature=0.3
     )
-    browser_tool_config = BrowserToolConfig(window_w=1280, window_h=720, keep_browser_open=True,inner_llm_model_config=inner_llm_model_config)
+    browser_tool_config = BrowserToolConfig(width=1280,
+                                            height=720,
+                                            headless=False,
+                                            keep_browser_open=True,
+                                            inner_llm_model_config=inner_llm_model_config)
     agent_config = BrowserAgentConfig(
         agent_name=Agents.BROWSER.value,
         llm_provider="openai",
@@ -39,23 +43,19 @@ if __name__ == '__main__':
     #     max_actions_per_step=10,
     #     max_steps=100,
     # )
-    
-
-    
-    
 
     task_config = {
         'max_steps': 100,
         'max_actions_per_step': 100
     }
 
-#     client.submit(
-#         GeneralTask(input="""step1: first go to https://www.dangdang.com/ and search for 'the little prince' and rank by sales from high to low, get the first 5 results and put the products info in memory.
-# step 2: write each product's title, price, discount, and publisher information to a fully structured HTML document with write_to_file, ensuring that the data is presented in a table with visible grid lines.
-# step3: open the html file in browser by go_to_url""",
-#                     swarm=Swarm(BrowserAgent(conf=agent_config)),
-#                     tools=[BrowserTool(conf=browser_tool_config)],
-#                     task_config=task_config))
+    #     client.submit(
+    #         GeneralTask(input="""step1: first go to https://www.dangdang.com/ and search for 'the little prince' and rank by sales from high to low, get the first 5 results and put the products info in memory.
+    # step 2: write each product's title, price, discount, and publisher information to a fully structured HTML document with write_to_file, ensuring that the data is presented in a table with visible grid lines.
+    # step3: open the html file in browser by go_to_url""",
+    #                     swarm=Swarm(BrowserAgent(conf=agent_config)),
+    #                     tools=[BrowserTool(conf=browser_tool_config)],
+    #                     task_config=task_config))
 
     # client.submit(
     #     GeneralTask(input="""访问www.baidu.com，搜索姚明的信息，找到他的百度百科介绍页，打开并将页面html存到本地""",
@@ -67,6 +67,6 @@ if __name__ == '__main__':
         Task(input="""step1: first go to https://www.dangdang.com/ and search for 'the little prince' and rank by sales from high to low, get the first 5 results and put the products info in memory.
     step 2: write each product's title, price, discount, and publisher information to a fully structured HTML document with write_to_file, ensuring that the data is presented in a table with visible grid lines.
     step3: open the html file in browser by go_to_url""",
-                    swarm=Swarm(BrowserAgent(conf=agent_config)),
-                    tools=[ToolFactory(Tools.BROWSER.value, conf=browser_tool_config)],
-                    task_config=task_config))
+             swarm=Swarm(BrowserAgent(conf=agent_config)),
+             tools=[ToolFactory(Tools.BROWSER.value, conf=browser_tool_config)],
+             task_config=task_config))
