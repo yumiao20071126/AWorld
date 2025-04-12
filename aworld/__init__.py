7320bcf92861d7e3698f97de88da222c112b1a2a
ganrunsheng <ganrunsheng@antgroup.com>
2025-04-12 22:17:51 +0800

    setup.py fix import package

--- aworld/__init__.py
+++ aworld/__init__.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 # Copyright (c) 2025 inclusionAI.
+from aworld.utils import import_packages
 
+import_packages(["dotenv", "python-dotenv"])
 from dotenv import load_dotenv
 
 load_dotenv()
 
-__version__ = '0.1.11'
+__version__ = '0.1.0'
