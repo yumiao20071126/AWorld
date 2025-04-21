417f13ca2db877159e93eb92bef7e669fd76974f
chunfeng.w <chunfeng.w@antgroup.com>
2025-04-21 11:09:47 +0800

    remove token

--- examples/trace/opentelemetry_trace.py
+++ examples/trace/opentelemetry_trace.py
@@ -1,12 +1,12 @@
+import os
 import aworld.trace as trace
-from examples.trace.autotrace_demo import TestClassB
 
 from aworld.logs.util import logger
 
 
 def main():
     trace.trace_configure(
-        backends=["console"],
+        backends=["logfire"],
         write_token="Your logfire write token, create guide refer to https://logfire.pydantic.dev/docs/how-to-guides/create-write-tokens/"
     )
 
@@ -21,11 +21,12 @@ def main():
             current_span = trace.get_current_span()
             logger.info("trace_id=%s", current_span.get_trace_id())
 
-    b = TestClassB()
-    b.classb_function_1()
-    b.classb_function_2()
-    b.classb_function_1()
-    b.classb_function_2()
+    # from examples.trace.autotrace_demo import TestClassB
+    # b = TestClassB()
+    # b.classb_function_1()
+    # b.classb_function_2()
+    # b.classb_function_1()
+    # b.classb_function_2()
 
 
 if __name__ == "__main__":
