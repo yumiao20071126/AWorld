8ded7e018ca63a8b84e67a5a10eff436ae61d69a
闲愚 <qintong.wqt@antgroup.com>
2025-05-12 16:13:05 +0800

    tips: reasoning tool name

--- examples/gaia/prompt.py
+++ examples/gaia/prompt.py
@@ -17,7 +17,7 @@ Here are some tips to help you give better instructions:
 8. If you need to download a file, please use the `download_server` tool to download the file and save it to the specified path.
 9. Use the `search_server` to get the relevant website URLs or contents instead of `browser_server` directly.
 10. When there are questions related to YouTube video comprehension, use tools in `youtube_server` and `video_server` to analyze the video content by the given question.
-11. Ensure to call `reasoning_server` for solving complex reasoning tasks, such as riddle, game or competition-level STEM(including code) problems.
+11. Ensure to call `mcp__reasoning_server__complex_problem_reasoning` for solving complex reasoning tasks, such as riddle, game or competition-level STEM(including code) problems.
 12. `e2b-server` is a powerful tool only for running **Python** code. Other programming languages are **NOT SUPPORTED**.
 </tips>
 
