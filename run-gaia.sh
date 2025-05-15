cb9d95ee620a7a6ea220ca77cd88bdef3015fd4c
文龙 <wenlong.yanwl@antgroup.com>
2025-05-15 20:10:27 +0800

    AWorld docker env

--- run-gaia.sh
+++ run-gaia.sh
@@ -8,7 +8,6 @@ fi
 # Check GAIA dataset
 if [ ! -d "examples/gaia/GAIA" ]; then
     echo "Please download GAIA dataset from https://huggingface.co/datasets/gaia-benchmark/GAIA and put it in examples/gaia/GAIA"
-    echo "git clone git@hf.co:datasets/gaia-benchmark/GAIA examples/gaia/GAIA"
     exit 1
 fi
 
