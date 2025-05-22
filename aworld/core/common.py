c9ea890d6f6029f69f4b9de3bc2d4acbfd1f79aa
wangchunf <chunfeng.w@antgroup.com>
2025-05-22 17:36:14 +0800

    Replay buffer storage (#86)
    replay buffer storage

--- aworld/core/common.py
+++ aworld/core/common.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2025 inclusionAI.
 
 from pydantic import BaseModel
-from typing import Dict, Any, Union, List
+from typing import Dict, Any, Optional, Union, List
 
 from aworld.config import ConfigDict
 from aworld.core.memory import MemoryItem
@@ -27,25 +27,25 @@ class Observation(BaseModel):
     It can be an agent(as a tool) in the swarm or a tool in the virtual environment.
     """
     # default is None, means the main virtual environment or swarm
-    container_id: str = None
+    container_id: Optional[str] = None
     # Observer who obtains observation, default is None for compatible, means an agent name or a tool name
-    observer: str = None
+    observer: Optional[str] = None
     # default is None for compatible, means with its action/ability name of an agent or a tool
     # NOTE: The only ability of an agent as a tool is handoffs
-    ability: str = None
+    ability: Optional[str] = None
     # The agent wants the observation to be created, default is None for compatible.
-    from_agent_name: str = None
+    from_agent_name: Optional[str] = None
     # To which agent should the observation be given, default is None for compatible.
-    to_agent_name: str = None
+    to_agent_name: Optional[str] = None
     # general info for agent
-    content: Any = None
+    content: Optional[Any] = None
     # dom_tree is a str or DomTree object
-    dom_tree: Union[str, Any] = None
-    image: str = None  # base64
-    action_result: List[ActionResult] = []
+    dom_tree: Optional[Union[str, Any]] = None
+    image: Optional[str] = None  # base64
+    action_result: Optional[List[ActionResult]] = []
     # for video or image list
-    images: List[str] = []
-    info: Dict[str, Any] = {}
+    images: Optional[List[str]] = []
+    info: Optional[Dict[str, Any]] = {}
 
 
 class StatefulObservation(Observation):
@@ -68,10 +68,10 @@ class ToolActionInfo(BaseModel):
 
 
 class ActionModel(BaseModel):
-    tool_name: str = None
+    tool_name: Optional[str] = None
     # agent name
-    agent_name: str = None
+    agent_name: Optional[str] = None
     # action_name is a tool action name by agent policy.
-    action_name: str = None
-    params: Dict[str, Any] = {}
-    policy_info: Any = None
+    action_name: Optional[str] = None
+    params: Optional[Dict[str, Any]] = {}
+    policy_info: Optional[Any] = None
