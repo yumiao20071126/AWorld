2ac2545a378a876cd257c135158205bb07ff8c11
道问 <aohui.xah@antgroup.com>
2025-06-03 10:41:40 +0800

    refactor retry time algorithm

--- aworld/config/conf.py
+++ aworld/config/conf.py
@@ -84,6 +84,7 @@ class ModelConfig(BaseModel):
     llm_client_type: ClientType = ClientType.SDK
     llm_sync_enabled: bool = True
     llm_async_enabled: bool = True
+    max_retries: int = 3
 
 
 class AgentConfig(BaseModel):
