2f70d3ff18daac93ac4509878f0ce8618e231f4c
文龙 <wenlong.yanwl@antgroup.com>
2025-06-16 14:43:24 +0800

    Add ray runtime env

--- run-server.sh
+++ run-server.sh
@@ -5,4 +5,8 @@ pip install -e . && \
 
 pip install -r mcp_servers/requirements.txt && \
 
+<<<<<<< Updated upstream
 cd examples/web && aworld web
+=======
+cd examples/command && aworld web
+>>>>>>> Stashed changes
