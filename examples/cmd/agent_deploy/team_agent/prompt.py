82488fef9a622217fe1d5cda7bf50c83f40de4b7
文龙 <wenlong.yanwl@antgroup.com>
2025-06-26 20:04:27 +0800

    new web ui

---
+++ examples/cmd/agent_deploy/team_agent/prompt.py
@@ -0,0 +1,13 @@
+search_sys_prompt = "YOU MUST SAY: 'Search Agent System Prompt'"
+
+search_agent_prompt = "YOU MUST SAY: 'Search Agent Prompt'"
+
+summary_sys_prompt = "YOU MUST SAY: 'Summary Agent System Prompt'"
+
+summary_agent_prompt = "YOU MUST SAY: 'Summary Agent Prompt'"
+
+
+output_sys_prompt = "YOU MUST SAY: 'Output Agent System Prompt'"
+
+
+output_agent_prompt = "YOU MUST SAY: 'Output Agent Prompt'"
