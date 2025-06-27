a0a702b43f79e07ab4af015a6bf80a00182e02d9
ganrunsheng <ganrunsheng@antgroup.com>
2025-06-27 13:34:57 +0800

    remove itself

--- aworld/core/agent/swarm.py
+++ aworld/core/agent/swarm.py
@@ -730,9 +730,11 @@ class TeamBuilder(TopologyBuilder):
             if pair[0] != root_agent:
                 agent_graph.add_edge(root_agent, pair[0])
                 root_agent.handoffs.append(pair[0].id())
+                pair[0].handoffs.remove(pair[0].id())
             else:
                 agent_graph.add_edge(root_agent, pair[1])
                 root_agent.handoffs.append(pair[1].id())
+                pair[1].handoffs.remove(pair[1].id())
         return agent_graph
 
 
