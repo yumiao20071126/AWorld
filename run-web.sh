5638c46aa110de892f41e7b896ea5885f0b57fde
文龙 <wenlong.yanwl@antgroup.com>
2025-07-09 14:38:48 +0800

    new web ui

--- run-web.sh
+++ run-web.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 BASE_DIR=$(dirname "$(readlink -f "$0")")
 
-cd $BASE_DIR/aworld/cmd/web/webui && npm run build
+cd $BASE_DIR
 
-cd $BASE_DIR && python setup.py install
+sh -c "cd aworld/cmd/web/webui && npm install && npm run build"
+
+python setup.py install
 
 cd examples/cmd && aworld web
