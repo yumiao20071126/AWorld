cd7e71f7f82f81fe21da4fc47a26866f04944e12
e68668 <8872258+eric68668@users.noreply.github.com>
2025-07-18 18:18:23 +0800

    new web ui (#311)

--- aworld/cmd/web/webui/src/pages/App.tsx
+++ aworld/cmd/web/webui/src/pages/App.tsx
@@ -855,7 +855,7 @@ const App: React.FC = () => {
             styles: {
               content: {
                 backgroundColor: '#f5f5f5',
-                maxWidth: '90%'
+                maxWidth: '800px'
               }
             }
           }))}
